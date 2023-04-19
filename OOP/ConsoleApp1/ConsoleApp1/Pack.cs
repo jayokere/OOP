6975f5be0cc59e96ce2146407ffbd5e130ddc619
jayokere <127126885+jayokere@users.noreply.github.com>
2023-04-19 20:45:03 +0100

    Add files via upload

---
+++ OOP/ConsoleApp1/ConsoleApp1/Pack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+
+public class Pack
+{
+    private List<Card> cards;
+    private Random random = new Random();
+
+    public Pack()
+    {
+        cards = new List<Card>();
+        for (int suit = 1; suit <= 4; suit++)
+        {
+            for (int value = 1; value <= 13; value++)
+            {
+                cards.Add(new Card(value, suit));
+            }
+        }
+    }
+
+    public void Shuffle()
+    {
+        for (int i = cards.Count - 1; i > 0; i--)
+        {
+            int j = random.Next(i + 1);
+            Card temp = cards[i];
+            cards[i] = cards[j];
+            cards[j] = temp;
+        }
+    }
+
+    public Card Deal()
+    {
+        if (cards.Count == 0) throw new InvalidOperationException("No more cards in the pack.");
+        Card card = cards[0];
+        cards.RemoveAt(0);
+        return card;
+    }
+}
---
+++ OOP/ConsoleApp1/ConsoleApp1/Tutorial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Tutorial
+{
+    public static void DisplayInstructions()
+    {
+        Console.WriteLine("Welcome to MathsTutor!");
+        Console.WriteLine("Here's how to play:");
+        Console.WriteLine("1. Choose an option from the menu.");
+        Console.WriteLine("2. Select '3 cards' or '5 cards' to get a math problem.");
+        Console.WriteLine("3. Enter your answer, and the program will check if it's correct.");
+        Console.WriteLine("4. You can quit the game by selecting 'Quit' from the menu.");
+    }
+}
